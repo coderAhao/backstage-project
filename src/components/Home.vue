42b40a3b609772075db42d3de37d8691ec970e61
xiaoyao <langjun>
2021-03-13 11:08:57 +0800

    change

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,18 @@
 <template>
-  <div class="home">
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      Header
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主题区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">Aside</el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -18,4 +29,18 @@ export default {
 </script>
 
 <style lang="less" scoped>
+  .home-container {
+    height: 100%;
+  }
+  .el-header {
+    background-color: #373d41;
+  }
+  .el-aside {
+    background-color: #333744;
+  }
+  .el-main {
+    background-color: #eaedf1;
+  }
+
+
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,17 +1,18 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
-// import { Form, FormItem } from 'element-ui'
-// import { Input } from 'element-ui'
-
-// 导入弹框组件 此组件与其他不同需挂载到Vue原型上以供全局使用
-import { Message } from 'element-ui'
-
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
 
+
+
+// 导入弹框Message组件 此组件与其他不同需挂载到Vue原型上以供全局使用
 // $message上有一系列方法可供使用
 Vue.prototype.$message = Message
